08b62bf479f6d8cb6ca25b51a8d24bb69dee3440
rirakkuma-ojisan <saidaniumetaro510@yahoo.com>
2021-01-25 20:50:31 +0900

    リソースベースルーティングとURLヘルパー

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get 'boards', to: 'boards#index'
-  post 'boards', to: 'boards#create'
-  get 'boards/new', to: 'boards#new'
-  get 'boards/:id', to: 'boards#show'
+  # get 'boards', to: 'boards#index'
+  # post 'boards', to: 'boards#create'
+  # get 'boards/new', to: 'boards#new'
+  # get 'boards/:id', to: 'boards#show'
+  resources :boards, only: [:index, :new, :create, :show]
 end
