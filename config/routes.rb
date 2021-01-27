c59382f2b9849ab2af7253ea253287b979894535
rirakkuma-ojisan <saidaniumetaro510@yahoo.com>
2021-01-27 17:11:03 +0900

    81. ユーザー認証機能で使用するルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,10 @@
 Rails.application.routes.draw do
-  get 'users/new'
 
-  get 'users/create'
+  # get 'sessions/create'
+  post 'login', to: 'sessions#create'
 
-  get 'users/me'
-
-  get 'home/index'
-
-  get 'sessions/create'
-
-  get 'sessions/destroy'
+  # get 'sessions/destroy'
+  delete 'logout', to: 'sessions#destroy'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   # get 'boards', to: 'boards#index'
@@ -20,6 +15,13 @@ Rails.application.routes.draw do
   #resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
   root 'home#index'
+
+  # get 'users/me'
+  get 'mypage', to: 'users#me'
+
+  # get 'users/new'
+  # get 'users/create'
+  resources :users, only: %i[new create]
 
   resources :boards
   resources :comments, only: %i[create destroy]
