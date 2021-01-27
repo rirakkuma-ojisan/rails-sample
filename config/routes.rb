5d0d0a278f871602e6330b418c1ea3ea262c616c
rirakkuma-ojisan <saidaniumetaro510@yahoo.com>
2021-01-27 15:59:58 +0900

    78.ヘッダーメニューの追加とヘルパーメソッドの作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   
   #resources :boards, only: [:index, :new, :create, :show, :edit, :update, :destroy]
 
+  root 'home#index'
+
   resources :boards
   resources :comments, only: %i[create destroy]
 end
