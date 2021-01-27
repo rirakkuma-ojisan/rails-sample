19216179b1a2bfd829dc7b0abf3b34f6560ede0b
rirakkuma-ojisan <saidaniumetaro510@yahoo.com>
2021-01-27 19:16:09 +0900

    85. ログイン、ログアウト機能の実装

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,18 @@
 class SessionsController < ApplicationController
   def create
+    user = User.find_by(name: params[:session][:name])
+    
+    # user.rbにhas_secure_passwordを記載したおかげで、authenticateメソッドが使える
+    # authenticate、メソッドはnameとpasswordの組み合わせが正しいかチェックする
+    if user && user.authenticate(params[:session][:name])
+      session[:user_id] = user.id
+      redirect_to mypage_path
+    else
+      render 'home/index'
   end
 
   def destroy
+    session.delete(:user_id)
+    redirect_to root_path
   end
 end
