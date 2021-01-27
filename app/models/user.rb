693685e964ecede236930e28d76fe6e5164ccdfc
rirakkuma-ojisan <saidaniumetaro510@yahoo.com>
2021-01-28 01:10:26 +0900

    94.Userモデルへのテスト対象メソッドを作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,7 @@
 # Table name: users
 #
 #  id              :integer          not null, primary key
+#  birthday        :date
 #  name            :string(255)      not null
 #  password_digest :string(255)      not null
 #  created_at      :datetime         not null
@@ -28,4 +29,9 @@ class User < ApplicationRecord
     }
   validates :password,
     length: {maximum: 8}
+
+  # 生年月日から現在の年齢を返す
+  def age 
+    0
+  end
 end
